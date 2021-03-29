e8280820b245fb679cdd976eb46d6b26d14133cc
René Dohmen <acidjunk@gmail.com>
2021-03-29 21:16:16 +0200

    Prettier + props

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,18 +2,25 @@ import React from 'react'
 
 import { Prefixes } from 'orchestrator-client-prefix-page'
 import 'orchestrator-client-prefix-page/dist/index.css'
-import '@elastic/eui/dist/eui_theme_light.css';
-import { EuiPage, EuiPageBody, EuiPageContent, EuiPageHeader } from "@elastic/eui";
+import '@elastic/eui/dist/eui_theme_light.css'
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageHeader
+} from '@elastic/eui'
 
 const App = () => {
-  return <EuiPage>
-    <EuiPageBody>
-      <EuiPageHeader>LIR Prefixes component example page</EuiPageHeader>
-      <EuiPageContent>
-        <Prefixes/>
-      </EuiPageContent>
-    </EuiPageBody>
-  </EuiPage>
+  return (
+    <EuiPage>
+      <EuiPageBody>
+        <EuiPageHeader>LIR Prefixes component example page</EuiPageHeader>
+        <EuiPageContent>
+          <Prefixes />
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+  )
 }
 
 export default App
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,30 +2,30 @@
  * Copyright 2020-2021 SURF.
  */
 
-import axios from "axios";
+import axios from 'axios'
 
 // import { ENV } from "../env";
 
 // const apiPath = ENV.BACKEND_URL + "/api/";
-const apiPath = "http://localhost:8080/api/";
+const apiPath = 'http://localhost:8080/api/'
 
 // basic configuration for axios.
 // the 'Authorization' header is set in
 // index.ts:setUser
 const axiosConfig = {
-    baseURL: apiPath,
-    headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        post: {
-            "Content-Type": "application/json",
-        },
-        put: {
-            "Content-Type": "application/json",
-        },
+  baseURL: apiPath,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    post: {
+      'Content-Type': 'application/json'
     },
-};
+    put: {
+      'Content-Type': 'application/json'
+    }
+  }
+}
 
-const axiosInstance = axios.create(axiosConfig);
+const axiosInstance = axios.create(axiosConfig)
 
-export default axiosInstance;
+export default axiosInstance
